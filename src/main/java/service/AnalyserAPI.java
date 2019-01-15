6254e6a4e8dd4e18afb61e196aa07150249c9359
Jacopo Soldani <jsoldani@gmail.com>
2019-01-15 16:32:41 +0100

    added alive method

--- src/main/java/service/AnalyserAPI.java
+++ src/main/java/service/AnalyserAPI.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/mm")
 @Produces(MediaType.APPLICATION_JSON)
@@ -16,8 +17,11 @@ public class AnalyserAPI {
     }
     
     @GET
-    public String isAlive() {
-        return aliveMsg;
+    public Response isAlive() {
+        return Response.ok()
+                .entity(aliveMsg)
+                .type(MediaType.TEXT_PLAIN)
+                .build();
     }
     
 }
